3cce0a2198748c46da6c777566314d358e211894
cryptodoctor <emmanuelneni@gmail.com>
2019-07-09 14:38:10 +0300

    added new avatars

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -3,8 +3,41 @@
     <h1 class="subheading grey--text">Team page</h1>
 
     <v-container grid-list-xs>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam unde libero, asperiores molestias facilis esse itaque, necessitatibus reprehenderit quis accusantium fuga adipisci neque voluptatem, tenetur velit placeat eos impedit possimus!</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora tenetur reprehenderit illo molestiae aperiam praesentium quidem. Harum eum quas veritatis atque architecto provident sit, at, sed id ullam ratione quo.</p>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+          <v-card class="text-xs-center ma-3">
+            <v-responsive class="pt-4">Image goes here</v-responsive>
+            <v-card-text>
+              <div class="subheading">{{person.name}}</div>
+              <div class="grey--text">{{person.role}}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-tooltip top>
+                <v-btn fab color="grey" small slot="activator">
+                  <v-icon small>message</v-icon>
+                </v-btn>
+                <span>Send a Message</span>
+              </v-tooltip>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      team: [
+        { name: "The Net Ninja", role: "Web developer" },
+        { name: "Ryu", role: "Graphic designer" },
+        { name: "Chun Li", role: "Web developer" },
+        { name: "Gouken", role: "Social media maverick" },
+        { name: "Yoshi", role: "Sales guru" }
+      ]
+    };
+  }
+};
+</script>
