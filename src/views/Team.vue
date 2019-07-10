0cbcef6f3240c1c8c50fb7a7f01c2c8ddb42bbdd
cryptodoctor <emmanuelneni@gmail.com>
2019-07-10 10:41:43 +0300

    added avatars for navigation and teams page

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -6,12 +6,16 @@
       <v-layout row wrap>
         <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
           <v-card class="text-xs-center ma-3">
-            <v-responsive class="pt-4">Image goes here</v-responsive>
+            <v-responsive class="pt-4">
+              <v-avatar size="100" class="grey lighten-2">
+                <img :src="person.avatar" :alt="person.name" />
+              </v-avatar>
+            </v-responsive>
             <v-card-text>
               <div class="subheading">{{person.name}}</div>
               <div class="grey--text">{{person.role}}</div>
             </v-card-text>
-            <v-card-actions>
+            <v-card-actions justify-space-around>
               <v-tooltip top>
                 <v-btn fab color="grey" small slot="activator">
                   <v-icon small>message</v-icon>
@@ -31,11 +35,19 @@ export default {
   data() {
     return {
       team: [
-        { name: "The Net Ninja", role: "Web developer" },
-        { name: "Ryu", role: "Graphic designer" },
-        { name: "Chun Li", role: "Web developer" },
-        { name: "Gouken", role: "Social media maverick" },
-        { name: "Yoshi", role: "Sales guru" }
+        {
+          name: "Neni Emsu",
+          role: "Web developer",
+          avatar: "/avatar-1.png"
+        },
+        { name: "Ryu", role: "Graphic designer", avatar: "/avatar-2.png" },
+        { name: "Chun Li", role: "Web developer", avatar: "/avatar-3.png" },
+        {
+          name: "Gouken",
+          role: "Social media maverick",
+          avatar: "/avatar-4.png"
+        },
+        { name: "Yoshi", role: "Sales guru", avatar: "/avatar-5.png" }
       ]
     };
   }
